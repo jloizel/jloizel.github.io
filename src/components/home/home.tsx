ca729d2e0900344ab4efb30fa5a748a67e5c5970
Jack <102869405+jloizel@users.noreply.github.com>
2024-06-14 15:35:01 +0100

    updates

---
+++ src/components/home/home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from "./home.module.css"
+
+function Home() {
+  return (
+    <div className={styles.home} id="home">
+      <div className={styles.header}>
+        JACK LOIZEL
+      </div>
+      <div className={styles.text}>
+
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from "./navbar.module.css"
+
+const scrollToSection = (sectionId: string) => {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+function NavBar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.nameContainer}>
+        JackLoizel<span style={{color: "white"}}>.</span>
+      </div>
+      <div className={styles.linksContainer}>
+        <div className={styles.link} onClick={() => scrollToSection("home")}>
+          // home
+        </div>
+        <div className={styles.link}>
+          // experience
+        </div>
+        <div className={styles.link}>
+          // resume
+        </div>
+        <div className={styles.link}>
+          // contact
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default NavBar;
