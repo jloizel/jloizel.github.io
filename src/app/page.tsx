aff9cc47e79d8585c7da786fd11fa9849bf5148e
Jack <102869405+jloizel@users.noreply.github.com>
2024-06-16 01:33:44 +0100

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import styles from "./app.module.css" 
+import styles from "./page.module.css" 
 import NavBar from "../../components/navbar/navbar"
 import Home from '../../components/home/home';
 
