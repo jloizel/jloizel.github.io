699cd64b7ad6bc6b79a2b967b9e6056c6c1a03d7
Jack <102869405+jloizel@users.noreply.github.com>
2024-06-25 21:21:55 +0100

    Update resume.tsx

--- components/resume/resume.tsx
+++ components/resume/resume.tsx
@@ -11,7 +11,7 @@ function Resume() {
           <IoIosArrowForward className={styles.icon}/>
           Résumé
         </div>
-        <div className={styles.text}>
+        <div className={styles.text}> 
           Throughout the past year, I have embarked on numerous projects that reflect my passion for coding. These selected works demonstrate my proficiency in various programming languages, frameworks, and tools. Each project presented unique challenges and opportunities for growth, allowing me to refine my skills and expand my knowledge. Take a look at these highlights to get a sense of my capabilities and accomplishments.
         </div>
       </div>
