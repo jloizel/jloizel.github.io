0804b4842e0bc606669ad371a4bfb420583d7bbd
Jack <102869405+jloizel@users.noreply.github.com>
2024-08-08 23:06:20 +0200

    updates to footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,7 @@ import { AiOutlineCopyright } from "react-icons/ai";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
-
+import { FaArrowUp } from "react-icons/fa";
 
 function Footer() {
 
@@ -15,15 +15,27 @@ function Footer() {
     }
   };
 
+  const jumpToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className={styles.footer}>
       <div className={styles.left}>
-        <AiOutlineCopyright/>
+        <AiOutlineCopyright className={styles.icon}/>
         Jack Loizel 2024. All rights reserved.
       </div>
       <div className={styles.right}>
-        <FaLinkedin/>
-        <FaGithub/>
+        <div className={styles.logoContainer}>
+          <FaLinkedin className={styles.logo} href="https://www.linkedin.com/in/jackloizel/" target="_blank"/>
+          <FaGithub className={styles.logo} href="https://github.com/jloizel" target='_blank'/>
+        </div>
+        <div className={styles.arrowContainer}>
+          <FaArrowUp className={styles.arrow} onClick={() => scrollToSection("home")}/>
+        </div>
       </div>
     </div>
   );
