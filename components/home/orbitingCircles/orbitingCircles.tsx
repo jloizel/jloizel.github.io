f0e52a9e9a90d96be30ed06988f880484eca1520
Jack <102869405+jloizel@users.noreply.github.com>
2024-08-11 22:50:36 +0200

    adding logos to circles

--- components/home/orbitingCircles/orbitingCircles.tsx
+++ components/home/orbitingCircles/orbitingCircles.tsx
@@ -1,24 +1,46 @@
 "use client"
 
 import React from 'react';
-import styles from "./orbitingCircles.module.css"
+import styles from "./orbitingCircles.module.css";
 import { FaLinkedin, FaNodeJs, FaReact } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import type { siTypescript } from 'simple-icons';
 
 
 function OrbitingCircles() {
 
   return (
     <div className={styles.circlesContainer}>
-      <a className={styles.circle1} href="https://github.com/jloizel" style={{textDecoration: "none"}}>
-        <FaGithub className={styles.logo1}/>
-      </a>
-      <a className={styles.circle2} href="https://www.linkedin.com/in/jackloizel/" style={{textDecoration: "none"}}>
-        <FaLinkedin className={styles.logo2}/>
-      </a>
-      <img height="32" width="32" src="/icons/typescript.svg" />
+      <div className={styles.circle1}>
+        <img height="32" width="32" src="/icons/css.svg" className={`${styles.logo1} ${styles.logo1_1}`} />
+      </div>
+      <div className={styles.circle1}>
+        <img height="32" width="32" src="/icons/html.svg" className={`${styles.logo1} ${styles.logo1_2}`} />
+      </div>
+      <div className={styles.circle1}>
+        <img height="32" width="32" src="/icons/javascript.svg" className={`${styles.logo1} ${styles.logo1_3}`} />
+      </div>
+      <div className={styles.circle1}>
+        <img height="32" width="32" src="/icons/mongodb.svg" className={`${styles.logo1} ${styles.logo1_4}`} />
+      </div>
+      <div className={styles.circle1}>
+        <img height="32" width="32" src="/icons/mongoose.svg" className={`${styles.logo1} ${styles.logo1_5}`} />
+      </div>
+      <div className={styles.circle1}>
+        <img height="32" width="32" src="/icons/python.svg" className={`${styles.logo1} ${styles.logo1_6}`} />
+      </div>
 
+      <div className={styles.circle2}>
+        <img height="32" width="32" src="/icons/npm.svg" className={`${styles.logo2} ${styles.logo2_1}`} />
+      </div>
+      <div className={styles.circle2}>
+        <img height="32" width="32" src="/icons/nodejs.svg" className={`${styles.logo2} ${styles.logo2_2}`}/>
+      </div>
+      <div className={styles.circle2}>
+        <img height="32" width="32" src="/icons/react.svg" className={`${styles.logo2} ${styles.logo2_3}`} />
+      </div>
+      <div className={styles.circle2}>
+        <img height="32" width="32" src="/icons/typescript.svg" className={`${styles.logo2} ${styles.logo2_4}`} />
+      </div>
     </div>
   );
 }
