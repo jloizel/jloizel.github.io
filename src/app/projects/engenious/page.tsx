dd89550f0e8120e5ac87497ebc3660cc89c1a448
Jack <jmloizel@gmail.com>
2024-08-23 22:59:55 +0100

    updates

---
+++ src/app/projects/engenious/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EngeniousPage = () => {
+  return (
+    <div>Engenious</div>
+  )
+}
+
+export default EngeniousPage
---
+++ src/app/projects/hoopstoglory/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HoopsToGloryPage = () => {
+  return (
+    <div>HoopsToGlory</div>
+  )
+}
+
+export default HoopsToGloryPage
