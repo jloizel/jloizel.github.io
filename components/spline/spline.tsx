9faa119bfd6b833afbe6d04f97a95d9559393f80
Jack <102869405+jloizel@users.noreply.github.com>
2024-09-05 22:33:12 +0100

    updates to home page spline scene

--- components/spline/spline.tsx
+++ components/spline/spline.tsx
@@ -5,8 +5,10 @@ const Spline = React.lazy(() => import('@splinetool/react-spline'));
 export default function SplineScene() {
   return (
     // <Spline scene="https://prod.spline.design/9nxhJkCellODUkIn/scene.splinecode" />
+    // <div>
       <Suspense fallback={<div>Loading...</div>}>
         <Spline scene="https://prod.spline.design/bI6wGt069uMkN3el/scene.splinecode" />
       </Suspense>
+      // </div>
   );
 }
