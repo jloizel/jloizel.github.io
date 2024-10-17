0b48d164e859ca8dd1d6bced085fe4f40978c8f7
Jack <jmloizel@gmail.com>
2024-10-17 22:51:21 +0100

    Update contactForm.tsx

--- components/contact/contactForm/contactForm.tsx
+++ components/contact/contactForm/contactForm.tsx
@@ -33,10 +33,10 @@ const ContactForm: FC = () => {
   const onSubmit = (data: FormData) => {
     if (formRef.current) {
       emailjs.sendForm(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || '',
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || '',
+        'Portfolio',
+        "Email Template",
         formRef.current,
-        process.env.NEXT_PUBLIC_EMAILJS_USER_ID || ''
+        "5sXrDZuoGKK_BsarB"
       ).then(
         (result) => {
           console.log('Email successfully sent!', result.text);
